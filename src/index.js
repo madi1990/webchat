dfd10b8e6b59a19a69eabcd7d93499263ba84abc
madi <madi@inmarket.com.au>
2021-04-15 16:49:05 +1000

    installed redis module change code as required by scalable websocket

--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ const path = require('path')
 const http = require('http')
 const express = require('express')
 const socketio = require('socket.io')
+const redis = require('redis')
 
 const app = express()
 const server = http.createServer(app)
 const io = socketio(server)
-const PORT = process.env.PORT || 80
+const PORT = process.env.PORT || 3000
 const publicdir = path.join(__dirname, '../public')
 app.use(express.static(publicdir))
 
@@ -17,21 +18,43 @@ app.get('/test', function (req, res) {
     res.send('hello world')
 })
 
-app.get('/chat', function (req, res) {
-    res.send('hello world')
+// redisClient will act as subscriber
+const redisClient = redis.createClient({
+    url: "redis://localhost:6379",
+    password: "1qaz2wsx#EDC"
 })
+
+const publisher = redisClient.duplicate()
+
+redisClient.subscribe(defaultRoom)
+
+redisClient.on('connect', function() {
+    console.log('redis connected');
+});
+
+redisClient.on('message', (channel, msg) => {
+    // Sub
+    if (channel === defaultRoom) {
+        // Send message to users
+        io.to(defaultRoom).emit("message", generatemsg(io.id, msg))
+    }
+});
 
 io.on('connection', (socket) => {
     console.log("new connection")
 
+    // User joins
     socket.on("join", ({ username, room }) => {
         socket.join(room)
         socket.emit("message", generatemsg("Admin", "Welcome!"))
-        socket.broadcast.to(room).emit("message", generatemsg("Admin", username + ` has joined!`))
+        //socket.broadcast.to(room).emit("message", generatemsg("Admin", username + ` has joined!`))
+        publisher.publish(room, username + ` has joined!`)
     })
 
+    // Receive message from users
     socket.on("sendMessage", (msg, callback) => {
-        io.to(defaultRoom).emit("message", generatemsg(socket.id, msg))
+        // Pub
+        publisher.publish(defaultRoom, msg)
         if(callback) callback()
     })
 })
